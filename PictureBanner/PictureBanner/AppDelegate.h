9c1ada4a6f253119fb5ce92201f090ad057c1bc4
姜守栋 <18511632756@163.com>
2017-10-09 11:14:26 +0800

    创建工程

---
+++ PictureBanner/PictureBanner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PictureBanner
+//
+//  Created by Abner on 2017/10/9.
+//  Copyright © 2017年 姜守栋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PictureBanner/PictureBanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PictureBanner
+//
+//  Created by Abner on 2017/10/9.
+//  Copyright © 2017年 姜守栋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
